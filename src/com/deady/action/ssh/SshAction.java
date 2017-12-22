c0a515b3fe93738c6734695e29457e550dd91ef9
wuwenzhe <wzwuwenzhe@163.com>
2017-12-22 13:51:15 +0800

    1.删掉一些没用的代码 2.新增远程关机接口

--- src/com/deady/action/ssh/SshAction.java
+++ src/com/deady/action/ssh/SshAction.java
@@ -1,40 +1,44 @@
 package com.deady.action.ssh;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.cnblogs.zxub.utils2.configuration.ConfigUtil;
 import com.deady.annotation.DeadyAction;
-import com.deady.utils.SSHHelper;
-import com.deady.utils.SSHResInfo;
-import com.jcraft.jsch.JSchException;
+import com.deady.common.FormResponse;
+import com.deady.utils.HttpClientUtil;
 
 @Controller
 public class SshAction {
+
+	private static PropertiesConfiguration conf = ConfigUtil
+			.getProperties("deady");
+	private static Logger log = LoggerFactory.getLogger(SshAction.class);
 
 	@RequestMapping(value = "/shutdownPC", method = RequestMethod.POST)
 	@DeadyAction(checkLogin = true)
 	public Object clientRegister(HttpServletRequest req, HttpServletResponse res)
 			throws Exception {
-		// 执行关机操作
-		try {
-			// 使用目标服务器机上的用户名和密码登陆
-			SSHHelper helper = new SSHHelper("192.168.31.213", 22, "root",
-					"wenzhebaba2");
-			String command = "shutdown -h now";
-			try {
-				SSHResInfo resInfo = helper.sendCmd(command);
-				System.out.println(resInfo.toString());
-				helper.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} catch (JSchException e) {
-			e.printStackTrace();
+		FormResponse response = new FormResponse(req);
+		String url = conf.getString("remote.url");
+
+		log.info("---------------调用远程关机--------------");
+		String back = HttpClientUtil.sendGet(url + "/shutdownPC",
+				new HashMap<String, Object>(), "UTF-8");
+		if (StringUtils.isEmpty(back)) {
+			log.info("back:" + back);
 		}
-		return null;
+		log.info("---------------调用结束--------------");
+		return response;
 	}
 }
